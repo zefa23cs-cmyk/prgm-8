9759733ab864c4f976251e73b0dc9ee29121914e
zefa23cs-cmyk <zefa23cs@cmrit.ac.in>
2026-03-09 09:27:44 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hii, I'm Zeba Fatma of C3 batch 6th semester. This is my first Maven Project. Have a good day!!" );
+    }
+}
